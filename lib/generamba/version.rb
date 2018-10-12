0495936263abf33ef64c1c9d0d094f4b465113a1
Siarhei Ladzeika <sergey.ladeiko@gmail.com>
2018-10-12 13:07:59 +0300

    bump version

--- lib/generamba/version.rb
+++ lib/generamba/version.rb
@@ -1,5 +1,5 @@
 module Generamba
-  VERSION = '1.5.1'
-  RELEASE_DATE = '5.07.2018'
+  VERSION = '1.5.2'
+  RELEASE_DATE = '12.10.2018'
   RELEASE_LINK = "https://github.com/ladeiko/Generamba/releases/tag/#{VERSION}"
 end
